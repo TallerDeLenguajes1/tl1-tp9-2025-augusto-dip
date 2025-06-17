9ad5e2a09068bdf3bcd1747db956c006667e533d
augusto-dip <augustodip26@gmail.com>
2025-06-17 10:51:05 -0300

    primera parte del punto 1

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+//using ;
+string pathPedido;
+bool existePath;
+
+do
+{
+    Console.WriteLine("Ingrese el path de un directorio que desea analizar: ");
+    pathPedido = Console.ReadLine();
+    existePath = Directory.Exists(pathPedido);
+    if(!existePath)
+    {
+        Console.WriteLine("el path o ruta al directorio no exite en el disco. Ingrese nuevamente una direccion valida.");
+    }
+} while(!existePath);
+
+
