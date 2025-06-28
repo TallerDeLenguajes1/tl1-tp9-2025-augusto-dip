21f0d8f78bf5b3ad72f36f74fbc65609545aac65
augusto-dip <augustodip26@gmail.com>
2025-06-28 14:04:47 -0300

    terminado ejercicio 1

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -10,10 +10,38 @@ do
     Console.WriteLine("Ingrese el path de un directorio que desea analizar: ");
     pathPedido = Console.ReadLine();
     existePath = Directory.Exists(pathPedido);
-    if(!existePath)
+    if (existePath)
+    {
+        foreach (var carpeta in Directory.GetDirectories(pathPedido))
+        {
+            string infoCarpeta = new DirectoryInfo(carpeta).Name;
+            Console.WriteLine(infoCarpeta);
+        }
+        foreach (var archivo in Directory.GetFiles(pathPedido))
+        {
+            FileInfo info = new FileInfo(archivo);
+            Console.WriteLine(info.Name + " - " + info.Length / 1024 + " KB");
+        }
+
+        string csv = pathPedido + @"\reporte_archivos.csv";
+
+        if (!File.Exists(csv))
+        {
+           using (FileStream fs = File.Create(csv)) { }   // aca lo habia hecho distinto pero me da error porque me dice q no se cierra el archivo una vez abierto. Antes asi:  File.Create(csv);
+        }
+
+        FileInfo[] infoArchivos = new DirectoryInfo(pathPedido).GetFiles();
+
+        using (StreamWriter sw = new(csv))
+        {
+            foreach (var info in infoArchivos)
+            {
+                sw.WriteLine(info.Name + "," + info.Length / 1024 + "," + info.LastWriteTime);
+            }
+        }
+    }
+    else
     {
         Console.WriteLine("el path o ruta al directorio no exite en el disco. Ingrese nuevamente una direccion valida.");
     }
 } while(!existePath);
-
-
--- LectorDirectorio/obj/Debug/net8.0/LectorDirectorio.AssemblyInfo.cs
+++ LectorDirectorio/obj/Debug/net8.0/LectorDirectorio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LectorDirectorio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ad5e2a09068bdf3bcd1747db956c006667e533d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LectorDirectorio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LectorDirectorio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
